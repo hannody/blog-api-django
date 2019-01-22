2b16839b9652dd81f6c0166dce60f54921d10bfd
Mohanad Abu Nayla <aljaboori@gmail.com>
2019-01-23 00:06:24 +0800

    Initial commit

---
+++ blogapi/blog_project/posts/permissions.py
@@ -0,0 +1,15 @@
+from rest_framework import permissions
+
+
+class IsAuthorOrReadOnly(permissions.BasePermission):
+
+
+#Create a custom permission that override the has_object_permission method
+
+    def has_object_permission(self, request, view, obj):
+        # Read-only permissions are allowed for any request
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        # Write permissions are only allowed to the author of a post
+        return obj.author == request.user
